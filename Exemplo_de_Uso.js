b3efdd7cab0411a703412ee7b2ecf7d61b87fafe
Eudes Silva | Transformando Linhas de Código em Soluções Inovadoras <eudsjs@gmail.com>
2024-09-13 09:08:48 -0300

    Exemplo de Uso

---
+++ Exemplo_de_Uso.js
@@ -0,0 +1,5 @@
+console.log(encontrarRecintos('leao', 1));  
+console.log(encontrarRecintos('macaco', 2));  
+console.log(encontrarRecintos('hipopotamo', 1)); 
+console.log(encontrarRecintos('elefante', 1));  
+console.log(encontrarRecintos('leao', 0));  
